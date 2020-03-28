a5f24b16117573398e37a2c71300c4184119a3c5
petezajot <petezajot@hotmail.com>
2020-03-28 14:00:33 -0600

    Commit inicial

---
+++ AppBeverages/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppBeverages
+//
+//  Created by Sergio Ricardo Garcia Vargas on 28/03/20.
+//  Copyright © 2020 Sergio Ricardo Garcia Vargas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
